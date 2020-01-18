d0b7ba2f6b643761b6f58fa8088b11d342206ce9
MPARASHA <mparasha@ualberta.ca>
2020-01-18 13:43:17 -0700

    Need to switch to window fml

--- Android/app/src/main/java/com/example/decom_android/MainActivity.java
+++ Android/app/src/main/java/com/example/decom_android/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.decom_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imageView = findViewById(R.id.imageView);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton Cam = findViewById(R.id.imageButton);
+        ImageButton Gall = findViewById(R.id.imageButton2);
+
+
+        Cam.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                dispatchTakePictureIntent();
+
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
 }
