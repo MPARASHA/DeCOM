3bbf3e6f413d3667962672678b5829f8af769eee
Manu Parashar <mparasha@ualberta.ca>
2020-01-18 14:42:01 -0700

    text recognizer naive Android API  Please enter the commit message for your changes. Lines starting

--- Android/app/src/main/java/com/example/decom_android/MainActivity.java
+++ Android/app/src/main/java/com/example/decom_android/MainActivity.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -42,6 +48,24 @@ public class MainActivity extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             imageView = findViewById(R.id.imageView);
             imageView.setImageBitmap(imageBitmap);
+            TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+            Frame imageFrame = new Frame.Builder()
+
+                    .setBitmap(imageBitmap)                 // your image bitmap
+                    .build();
+            String imageText = "";
+
+
+            SparseArray<TextBlock> textBlocks = textRecognizer.detect(imageFrame);
+
+            for (int i = 0; i < textBlocks.size(); i++) {
+                TextBlock textBlock = textBlocks.get(textBlocks.keyAt(i));
+                imageText = textBlock.getValue();                   // return string
+            }
+            TextView imageTextView = findViewById(R.id.textView);
+            imageTextView.setText(imageText);
+
+
         }
     }
 
