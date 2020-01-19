797641767eaa6b4141c56cc899309ae2b33b75a9
Manu Parashar <mparasha@ualberta.ca>
2020-01-18 17:09:35 -0700

    NENE

--- Android/app/src/main/java/com/example/decom_android/MainActivity.java
+++ Android/app/src/main/java/com/example/decom_android/MainActivity.java
@@ -27,7 +27,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_TAKE_PHOTO = 1;
     String mCurrentPhotoPath;
     ImageView imageView;
@@ -83,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                             TextView imageTextView = findViewById(R.id.textView);
                             imageTextView.setText(imageText);
+                            imageTextView.setVisibility(View.VISIBLE);
                         }
                     }
                     break;
